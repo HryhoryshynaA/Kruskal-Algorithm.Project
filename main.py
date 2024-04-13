48c237c1c35fc5ff2258bdbb9436933badc7414e
Adriana Hryhoryshyna <ahryhoryshyna@kse.org.ua>
2024-04-13 21:26:08 +0300

    union method done, and variables renamed (Anna can't spell :)))

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ class Graph:
         self.adjacency_matrix = ""   # матриця суміжності
         self.adjacency_list = "" # список суміжності
 
-    def find_absolute_parent(self, verce, parent):#рекурсивний пошук абсолютного батька/матері(жарт)
-        if parent[verce] == -1:
-            return verce
-        self.find_absolute_parent(parent[verce], parent)
+    def find_absolute_parent(self, vertice, parent):#рекурсивний пошук абсолютного батька/матері(жарт)
+        if parent[vertice] == -1:
+            return vertice
+        self.find_absolute_parent(parent[vertice], parent)
 
-    def union(self):
-        return "updated set"
+    def union(self, vertice_a, vertice_b, parent, rank):
+        root_vertice_a = self.find_absolute_parent(parent, vertice_a)
+        root_vertice_b = self.find_absolute_parent(parent, vertice_b)
+        if rank[root_vertice_a] == rank[vertice_b]: # якщо один ранк, то тоді ми самі обираємо з якої на яку вершину посилатися
+            parent[vertice_b] = root_vertice_a
+            rank[root_vertice_a] += 1 #тому що ранки одинакові при обʼєднанні ранк потрбіно збільшити на одиницю
+        elif rank[root_vertice_a] > rank[root_vertice_b]: # порівнюємо ранки щоб ми знали що буде новим абсолютним рутом
+            parent[root_vertice_b] = root_vertice_a
+        elif rank[root_vertice_a] < rank[root_vertice_b]:
+            parent[root_vertice_a] = root_vertice_b
+
 
     def kruskal(self):
         return "smallest spanning tree"
