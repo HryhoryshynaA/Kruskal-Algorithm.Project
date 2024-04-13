64a3efb5d247bcab94b382fe342ab63d0f15fc34
Adriana Hryhoryshyna <ahryhoryshyna@kse.org.ua>
2024-04-13 22:10:38 +0300

    adjacency list made for the graph

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ class Graph:
         self.vertices = vertices # вершини
         self.edges = [] # ребра
         self.adjacency_matrix = []   # матриця суміжності
-        self.adjacency_list = "" # список суміжності
-
         for i in range(len(vertices)):
             row = []
             for j in range(len(vertices)):
                 row.append(0)
             self.adjacency_matrix.append(row)
 
+        self.adjacency_list = {v: [] for v in vertices}  # список суміжності, де будуть дані по кожній вершині + ваги
+
     def add_edge(self, v1, v2, weight):
         self.adjacency_matrix[v1][v2] = weight
         self.adjacency_matrix[v2][v1] = weight
+        self.adjacency_list[v1].append((v2, weight))
+        self.adjacency_list[v2].append((v1, weight))
 
     def find_absolute_parent(self, verce, parent): #рекурсивний пошук абсолютного батька/матері(жарт)
         if parent[verce] == -1:
