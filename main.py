0f93fbcd6ac5707d11749b6a7f5b538af4187aac
Adriana Hryhoryshyna <ahryhoryshyna@kse.org.ua>
2024-04-13 20:52:01 +0300

    methods that need to be done

---
+++ main.py
@@ -0,0 +1,16 @@
+class Graph:
+    def __init__(self, vertices):
+        self.vertices = vertices # вершини
+        self.edges = [] # ребра
+        self.adjacency_matrix = ""   # матриця суміжності
+        self.adjacency_list = "" # список суміжності
+
+    def absolute_parent(self):
+        return "absolute parent"
+
+    def union(self):
+        return "updated set"
+
+    def kruskal(self):
+        return "smallest spanning tree"
+
