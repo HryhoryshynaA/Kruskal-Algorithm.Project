4d0b00bdece40e6dda29a020fb925fe9a9cd7444
Adriana Hryhoryshyna <ahryhoryshyna@kse.org.ua>
2024-04-13 22:21:21 +0300

    union method that was done before restored

--- main.py
+++ main.py
@@ -22,8 +22,18 @@ class Graph:
             return verce
         self.find_absolute_parent(parent[verce], parent)
 
-    def union(self):
-        return "updated set"
+    def union(self, vertice_a, vertice_b, parent, rank):
+        root_vertice_a = self.find_absolute_parent(parent, vertice_a)
+        root_vertice_b = self.find_absolute_parent(parent, vertice_b)
+        if rank[root_vertice_a] == rank[
+            vertice_b]:  # якщо один ранк, то тоді ми самі обираємо з якої на яку вершину посилатися
+            parent[vertice_b] = root_vertice_a
+            rank[root_vertice_a] += 1  # тому що ранки одинакові при обʼєднанні ранк потрбіно збільшити на одиницю
+        elif rank[root_vertice_a] > rank[
+            root_vertice_b]:  # порівнюємо ранки щоб ми знали що буде новим абсолютним рутом
+            parent[root_vertice_b] = root_vertice_a
+        elif rank[root_vertice_a] < rank[root_vertice_b]:
+            parent[root_vertice_a] = root_vertice_b
 
     def kruskal(self):
         return "smallest spanning tree"
