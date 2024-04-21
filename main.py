2ba1a18f91f10d2ffb2a52d434c7921a85755172
Anna Oleksandrovych <150846802+anwalv@users.noreply.github.com>
2024-04-21 07:51:38 +0300

    add kruskal algorithm

--- main.py
+++ main.py
@@ -1,54 +1,70 @@
 import random
-
+import matplotlib
+import networkx
 class Graph:
     def __init__(self, vertices):
         self.vertices = vertices # вершини
-        self.edges = [] # ребра
+        self.edges_record = [] # основна структура для зберігання
         self.adjacency_matrix = []   # матриця суміжності
         for i in range(len(vertices)):
             row = []
             for j in range(len(vertices)):
                 row.append(0)
             self.adjacency_matrix.append(row)
-
         self.adjacency_list = {v: [] for v in vertices}  # список суміжності, де будуть дані по кожній вершині + ваги
-
     def add_edge(self, v1, v2, weight):
+        self.edges_record.append((v1, v2, weight))
         self.adjacency_matrix[v1][v2] = weight
         self.adjacency_matrix[v2][v1] = weight
-        self.adjacency_list[v1].append((v2, weight))
-        self.adjacency_list[v2].append((v1, weight))
-
-    def find_absolute_parent(self, verce, parent): #рекурсивний пошук абсолютного батька/матері(жарт)
-        if parent[verce] == -1:
-            return verce
-        self.find_absolute_parent(parent[verce], parent)
-
-    def union(self, vertice_a, vertice_b, parent, rank):
-        root_vertice_a = self.find_absolute_parent(parent, vertice_a)
-        root_vertice_b = self.find_absolute_parent(parent, vertice_b)
-        if rank[root_vertice_a] == rank[vertice_b]:  # якщо один ранк, то тоді ми самі обираємо з якої на яку вершину посилатися
-            parent[vertice_b] = root_vertice_a
-            rank[root_vertice_a] += 1  # тому що ранки одинакові при обʼєднанні ранк потрбіно збільшити на одиницю
-        elif rank[root_vertice_a] > rank[root_vertice_b]:  # порівнюємо ранки щоб ми знали що буде новим абсолютним рутом
-            parent[root_vertice_b] = root_vertice_a
-        elif rank[root_vertice_a] < rank[root_vertice_b]:
-            parent[root_vertice_a] = root_vertice_b
-
-    def kruskal(self):
-        return "smallest spanning tree"
 
 
+    def find_absolute_parent(self, vertex, parent): #рекурсивний пошук абсолютного батька/матері(жарт)
+        if parent[vertex] == -1:
+            return vertex
+        return self.find_absolute_parent(parent[vertex], parent)
 
-def generate_graph(num_vertices, density):
-    vertices = list(range(num_vertices))
-    graph = Graph(vertices)
-    max_edges = num_vertices * (num_vertices - 1) / 2
-    probability = density / max_edges # формула за моделлю Ердеша — Реньї
+    def union(self, vertex_a, vertex_b, parent, rank):
+        root_vertex_a = self.find_absolute_parent( vertex_a, parent)
+        root_vertex_b = self.find_absolute_parent(vertex_b, parent)
+        if rank[root_vertex_a] == rank[vertex_b]:  # якщо однаковий ранк, то тоді ми самі обираємо з якої на яку вершину посилатися
+            parent[vertex_b] = root_vertex_a
+            rank[root_vertex_a] += 1  # тому що ранки одинакові при обʼєднанні ранк потрбіно збільшити на одиницю
+        elif rank[root_vertex_a] > rank[root_vertex_b]:  # порівнюємо ранки щоб ми знали що буде новим абсолютним рутом
+            parent[root_vertex_b] = root_vertex_a
+        elif rank[root_vertex_a] < rank[root_vertex_b]:
+            parent[root_vertex_a] = root_vertex_b
 
-    for u in range(num_vertices):
-        for v in range(u + 1, num_vertices):
-            if random.random() < probability: #генеруємо рандомне число між 0 і 1. Якщо менше то "успіх"
-                weight = random.randint(1, 50)
-                graph.add_edge(u, v, weight)
-    return graph
+    def generate_graph(num_vertices, density):
+        vertices = list(range(num_vertices))
+        graph = Graph(vertices)
+        max_edges = num_vertices * (num_vertices - 1) / 2
+        max_possible_edges = max_edges * density
+        for u in range(num_vertices):
+            for v in range(u + 1, num_vertices):
+                if random.random() < max_possible_edges / max_edges:
+                    weight = random.randint(1, 50)
+                    graph.add_edge(u, v, weight)
+        return graph
+
+
+def kruskal(graph):
+    sorted_edges = sorted(graph.edges_record, key=lambda x: x[2])
+    smallaest_spanning_tree = set()
+    parent = {}
+    rank = {}
+    for v in graph.vertices:
+        parent[v] = -1
+        rank[v] = 0
+    for edge in sorted_edges:
+        v1, v2, w = edge
+        root_v1 = graph.find_absolute_parent(v1, parent)
+        root_v2 = graph.find_absolute_parent(v2, parent)
+        if root_v1 != root_v2:
+            smallaest_spanning_tree.add((v1, v2, w))
+            graph.union(v1, v2, parent, rank)
+    return smallaest_spanning_tree
+
+graph = Graph.generate_graph(10, 0.8)
+minimum_spanning_tree = kruskal(graph)
+print(graph.edges_record)
+print(minimum_spanning_tree)
